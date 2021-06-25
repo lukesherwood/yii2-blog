fbd425a83d7ecb2f34b3c4f4e8a842c700d5f6da
Luke Sherwood <58277041+lukesherwood@users.noreply.github.com>
2021-06-25 10:05:42 -0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=blog',
     'username' => 'lukesherwood',
-    'password' => 'starish55',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
